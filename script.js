6059144a063fb6426f15e6c71b3e865fb9a7ff80
wahuu2 <lilywahu1@gmail.com>
2026-05-29 12:34:00 +0300

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const apiKey = "48174ef46d98cf149de2eebb801c2f77";
- const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=germany"; 
- async function checkWeather() {
-    const response = await fetch(`${apiUrl}&appid=${apiKey}`);
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox=document.querySelector(".search input");
+const searchBtn=document.querySelector(".search button");
+
+async function checkWeather(city) {
+    const response = await fetch(`${apiUrl}${city}&appid=${apiKey}`);
     const data = await response.json();
     console.log(data);
+
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " °C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 }
+
+
 checkWeather();
