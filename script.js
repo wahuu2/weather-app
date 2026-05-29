18b24f186f5022e3adb678d6a55513cc18907b02
wahuu2 <lilywahu1@gmail.com>
2026-05-29 13:00:24 +0300

    script.js

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ const weatherIcon=document.querySelector(".weather-icon");
 async function checkWeather(city) {
     const response = await fetch(`${apiUrl}${city}&appid=${apiKey}`);
     const data = await response.json();
-    console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " °C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    
     if(data.weather[0].main=="Clouds"){
         weatherIcon.src="images/clouds.png";
     }else if(data.weather[0].main=="Clear"){
@@ -25,6 +25,8 @@ async function checkWeather(city) {
     }else if(data.weather[0].main=="Mist"){
         weatherIcon.src="images/mist.png";
     }
+
+    document.querySelector(".weather").style.display="block";
 }
 
 searchBtn.addEventListener("click",()=>{
