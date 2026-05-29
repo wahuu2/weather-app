095a1ac081ef7b0a2e27aa68b74e5c35e6265fd0
wahuu2 <lilywahu1@gmail.com>
2026-05-29 12:29:32 +0300

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-        const apiKey = "48174ef46d98cf149de2eebb801c2f77";
-        const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=germany";
-
-        async function checkWeather() {
-            const response = await fetch(`${apiUrl}&appid=${apiKey}`);
-            const data = await response.json();
-            console.log(data);
-
-            document.querySelector(".city").innerHTML = data.name;
-            document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " °C";
-            document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-            document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-        }
-
-        checkWeather();
+const apiKey = "48174ef46d98cf149de2eebb801c2f77";
+ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=germany"; 
+ async function checkWeather() {
+    const response = await fetch(`${apiUrl}&appid=${apiKey}`);
+    const data = await response.json();
+    console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " °C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+}
+checkWeather();
